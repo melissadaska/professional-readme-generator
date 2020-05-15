8ab09d063f42f21f6fe2536645fd533b60797905
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-05-15 17:51:55 -0500

    add questions

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    return inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {
+          type: 'input',
+          name: 'user',
+          message: 'What is your GitHub user name?'
+      },
+      {
+          type: 'input',
+          name: 'repo',
+          message: 'What is your repository name?'
+      },
+      {
+          type: 'input',
+          name: 'install',
+          message: 'What are the steps required to install your project?'
+      },
+      {
+          type: 'input',
+          name: 'usage',
+          message: 'Provide instructions and examples for use. Include screenshots as needed.'
+      },
+      {
+          type: 'input',
+          name: 'credits',
+          message: 'Are there any collaborators?'
+          // if yes, include links to GitHub profiles
+      },
+      {
+          type: 'input',
+          name: 'license',
+          message: 'What is the license?'
+      },
+      {
+          type: 'checkbox',
+          name: 'badges',
+          message: 'Are there any badges?'
+          // if yes, include badges
+      },
+      {
+          type: 'input',
+          name: 'contrib',
+          message: 'Would you like others to contribute?'
+          // if yes, add guidelines on how to do so
+      },
+      {
+          type: 'input',
+          name: 'tests',
+          message: 'Do you have any tests written for your application?'
+      }
+    ])
+};
+
+promptUser()
   .then(answers => {
     // Use user feedback for... whatever!!
   })
@@ -14,10 +67,10 @@ inquirer
     }
   });
 
-// array of questions for user
-const questions = [
+// // array of questions for user
+// const questions = [
 
-];
+// ];
 
 // function to write README file
 function writeToFile(fileName, data) {
