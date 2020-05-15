b8068c8164ecc809b7183a4c79f2430c57de14b0
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-05-15 16:43:39 -0500

    add inquire package

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+
 // array of questions for user
 const questions = [
 
